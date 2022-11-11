84488e5ef7c0bed9c56a5f429e5759d1bb53d37f
Fang Yuan <yuanfang@alauda.io>
2022-11-11 18:41:36 +0800

    adapter istio bash

--- deploy/istio/adapt.sh
+++ deploy/istio/adapt.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 kubectl get -n istio-system cm istio-1-14 -ojson > adaptor.json
-echo -e "import json\nwith open('adaptor.json','r') as ofile:\n\to_data = json.load(ofile);\n\to_data['data']['mesh']= 'configSources:\\\n- address: xds://nacos-cs:18848\\\n' + o_data['data']['mesh']\nwith open('adaptor_.json','w') as dfile:\n\tjson.dump(o_data,dfile)" > patch.py
+echo -e "import json\nwith open('adaptor.json','r') as ofile:\n\to_data = json.load(ofile);\n\to_data['data']['mesh']= 'configSources:\\\n- address: xds://nacos-cs.default:18848\\\n' + o_data['data']['mesh']\nwith open('adaptor_.json','w') as dfile:\n\tjson.dump(o_data,dfile)" > patch.py
 python patch.py
 kubectl -n istio-system apply -f adaptor_.json
 kubectl delete -n istio-system "$(kubectl get -n istio-system -l app=istiod po -oname)"
